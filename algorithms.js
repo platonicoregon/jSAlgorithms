adb1427262b6e78bbea80b7f625b21a852347107
Zaporozhchenko Ivan <platonicoregon@gmail.com>
2018-03-18 14:40:22 +0200

    Find the Longest Word in a String and Title Case a Sentence tasks

--- algorithms.js
+++ algorithms.js
@@ -1,57 +1,37 @@
-// Reverse a String
+// Find the Longest Word in a String
 
-function reverseString(str) {
-	str = str.split("").reverse().join("");
-	return str;
-}
-
-reverseString("hello");
-
-reverseString("Howdy");
-
-reverseString("Greetings from Earth");
-
-
-//Reverse a String (reverse method as function)
-
-function reverseString(str) {
-	function myReverse(arr) {
-		var i,
-			newArr = new Array();
-		for(i = 0; i < arr.length; i++) {
-			newArr[(arr.length-1)-i] = arr[i];
+function findLongestWord(str) {
+	var i,
+		arrOfStr = str.split(" ");
+	for(i = 0, str = arrOfStr[0]; i < arrOfStr.length; i += 1) {
+		if(str.length < arrOfStr[i].length) {
+			str = arrOfStr[i];
 		}
-		return newArr;
 	}
-	str = myReverse(str.split("")).join("");
-	return str;
+	return str.length;
 }
 
-reverseString("hello");
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+console.log(findLongestWord("May the force be with you"));
+console.log(findLongestWord("Google do a barrel roll"));
+console.log(findLongestWord("What is the average airspeed velocity of an unladen swallow"));
+console.log(findLongestWord("What if we try a super-long word such as otorhinolaryngology"));
 
-reverseString("Howdy");
 
-reverseString("Greetings from Earth");
+//Title Case a Sentence
 
-
-//Factorialize a Number
-
-function factorialize(num) {
-	var res;
-
-	if(num <= 0) {
-		return 1;
+function titleCase(str) {
+	var i,
+		j,
+		arrOfStr = str.split(" ");
+	for(i = 0; i < arrOfStr.length; arrOfStr[i] = arrOfStr[i].join(""), i += 1) {
+		arrOfStr[i] = arrOfStr[i].split("");
+		arrOfStr[i][0] = arrOfStr[i][0].toUpperCase();
+		for(j = 1; j < arrOfStr[i].length; arrOfStr[i][j] = arrOfStr[i][j].toLowerCase(), j += 1) {}
 	}
-	else {
-		res = num * factorialize(num-1);
-		return res;
-	}
+	return arrOfStr.join(" ");
 }
 
-factorialize(5);
-
-factorialize(10);
-
-factorialize(20);
-
-factorialize(0);
+console.log(titleCase("I'm a little tea pot"));
+console.log(titleCase("sHoRt AnD sToUt"));
+console.log(titleCase("HERE IS MY HANDLE HERE IS MY SPOUT"));
